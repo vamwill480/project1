944834fc7aaf26ce742472d5051ad8a791590a8a
vamwill480 <vamwill480@gmail.com>
2025-04-08 09:02:02 +0000

    Add 27.py

---
+++ src/27.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://example.com"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+title = soup.find('h1').text
+print(title)
