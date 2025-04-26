eefc20ac5315ffc08452bf465c51d2838cab4765
vamwill480 <vamwill480@gmail.com>
2025-04-26 10:45:12 +0000

    Add 33.py

---
+++ src/33.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(a, b):
+    return a * b
+
+print("The sum of 5 and 3 is:", add_numbers(5, 3))
+print("The product of 2 and 4 is:", multiply_numbers(2, 4))
+
